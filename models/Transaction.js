d65aaf28fc9f96378269607a829ebb91d13aa930
ar-narola <ar@narola.email>
2018-04-28 12:56:52 +0530

    checkout process

--- models/Transaction.js
+++ models/Transaction.js
@@ -5,7 +5,20 @@ var Schema = mongoose.Schema;
 var TransactionSchema = new Schema({
     promoter_id: { type: mongoose.Schema.Types.ObjectId, ref: 'promoters', required: true },
     name: { type: String, required: true },
-    created_at: { type: Date, default: Date.now }
+    company: { type: String },
+    email: { type: String, required: true },
+    abn: { type: String, required: true },
+    total_amount: {type: Number},
+    country: { type: mongoose.Schema.Types.ObjectId, ref: 'country', required: true },
+    address_line1: { type: String, required: true },
+    address_line2: { type: String },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    post_code: { type: String, required: true },
+    credit_card: { type: String, required: true },
+    stripe_charge_id: {type: String},
+    status: { type: String, enum: ["pending", "paid", "failed"], default: "pending" },
+    created_at: { type: Date, default: Date.now },
 }, { versionKey: false });
 
 var Transaction = mongoose.model('transactions', TransactionSchema, 'transactions');
