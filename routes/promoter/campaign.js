0c0916ebc10a6343ccb15a1471c1b5bcc1041d1d
hidnarola <hda@narola.email>
2018-05-01 17:54:44 +0530

    no message

--- routes/promoter/campaign.js
+++ routes/promoter/campaign.js
@@ -704,43 +704,57 @@ router.post('/calendar', async (req, res) => {
 
 router.post("/filtered_campaign", async (req, res) => {
     user_id = req.userInfo.id;
-    logger.trace("Get all campaign API called");
     var filter = {};
     var match_filter = {};
-        var sort = {};
-        if (req.body.filter) {
-            req.body.filter.forEach(filter_criteria => {
-                if (filter_criteria.type === "exact") {
-                    match_filter[filter_criteria.field] = filter_criteria.value;
-                } else if (filter_criteria.type === "between") {
-                    if (filter_criteria.field === "age") {
-                        // Age is derived attribute and need to calculate based on date of birth
-                        match_filter[filter_criteria.field] = {
-                            "$lte": moment().subtract(filter_criteria.min_value, "years").toDate(),
-                            "$gte": moment().subtract(filter_criteria.max_value, "years").toDate()
-                        };
-                    } else {
-                        match_filter[filter_criteria.field] = { "$lte": filter_criteria.min_value, "$gte": filter_criteria.max_value };
-                    }
-                } else if (filter_criteria.type === "like") {
-                    var regex = new RegExp(filter_criteria.value);
-                    match_filter[filter_criteria.field] = { "$regex": regex, "$options": "i" };
-                } else if (filter_criteria.type === "id") {
-                    match_filter[filter_criteria.field] = { "$eq": new ObjectId(filter_criteria.value) };
+    var sort = {};
+
+    if (req.body.filter) {
+        req.body.filter.forEach(filter_criteria => {
+            if (filter_criteria.type === "exact") {
+                match_filter[filter_criteria.field] = filter_criteria.value;
+            } else if (filter_criteria.type === "between") {
+                if (filter_criteria.field === "age") {
+                    // Age is derived attribute and need to calculate based on date of birth
+                    match_filter[filter_criteria.field] = {
+                        "$lte": moment().subtract(filter_criteria.min_value, "years").toDate(),
+                        "$gte": moment().subtract(filter_criteria.max_value, "years").toDate()
+                    };
+                } else {
+                    match_filter[filter_criteria.field] = { "$lte": filter_criteria.min_value, "$gte": filter_criteria.max_value };
                 }
-            });
-        }
+            } else if (filter_criteria.type === "like") {
+                var regex = new RegExp(filter_criteria.value);
+                match_filter[filter_criteria.field] = { "$regex": regex, "$options": "i" };
+            } else if (filter_criteria.type === "id") {
+                match_filter[filter_criteria.field] = { "$eq": new ObjectId(filter_criteria.value) };
+            }
+        });
+    }
 
 
-        let keys = {
-            "fb_friends": "facebook.no_of_friends",
-            "insta_followers": "instagram.no_of_followers",
-            "twitter_followers": "twitter.no_of_followers",
-            "pinterest_followers": "pinterest.no_of_followers",
-            "linkedin_connection": "linkedin.no_of_connections",
-            "age": "dob"
+    let keys = {
+            "fb_friends": "user.facebook.no_of_friends",
+            "insta_followers": "user.instagram.no_of_followers",
+            "twitter_followers": "user.twitter.no_of_followers",
+            "pinterest_followers": "user.pinterest.no_of_followers",
+            "linkedin_connection": "user.linkedin.no_of_connections",
+            "year_in_industry": "user.experience",
+            "age": "user.date_of_birth",
+
+            "name": "user.name",
+            "gender": "user.gender",
+            "location": "user.location",
+            "job_industry": "user.job_industry",
+            "education": "user.education",
+            "language": "user.language",
+            "ethnicity": "user.ethnicity",
+            "interested_in": "user.interested_in",
+            "relationship_status": "user.relationship_status",
+            "music_taste": "user.music_taste"
         };
-        match_filter = await global_helper.rename_keys(match_filter, keys);
+
+
+    match_filter = await global_helper.rename_keys(match_filter, keys);
     var resp_data = await campaign_helper.get_filtered_campaign_by_promoter(user_id, match_filter);
 
     if (resp_data.status == 0) {
