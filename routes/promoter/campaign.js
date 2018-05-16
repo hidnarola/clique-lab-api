4354fdba4c549b5bd074500517c3eb7dc2050cbd
ar-narola <ar@narola.email>
2018-05-16 17:56:43 +0530

    temp API on

--- routes/promoter/campaign.js
+++ routes/promoter/campaign.js
@@ -728,102 +728,102 @@ router.post('/get_demographics', async (req, res) => {
  * /promoter/campaign/:campaign_id
  * Developed by "ar"
  */
-router.post('/:campaign_id',async(req,res) => {
-    var schema = {
-        'page_size': {
-            notEmpty: true,
-            errorMessage: "Page size is required"
-        },
-        'page_no': {
-            notEmpty: true,
-            errorMessage: "Page number is required"
-        }
-    };
-    req.checkBody(schema);
-    const errors = req.validationErrors();
-    if (!errors) {
-        var match_filter = {};
-        var sort = {};
-        if (req.body.filter) {
-            req.body.filter.forEach(filter_criteria => {
-                if (filter_criteria.type === "exact") {
-                    match_filter[filter_criteria.field] = filter_criteria.value;
-                } else if (filter_criteria.type === "between") {
-                    if (filter_criteria.field === "age") {
-                        // Age is derived attribute and need to calculate based on date of birth
-                        match_filter[filter_criteria.field] = {
-                            "$lte": moment().subtract(filter_criteria.min_value, "years").toDate(),
-                            "$gte": moment().subtract(filter_criteria.max_value, "years").toDate()
-                        };
-                    } else {
-                        match_filter[filter_criteria.field] = { "$lte": filter_criteria.min_value, "$gte": filter_criteria.max_value };
-                    }
-                } else if (filter_criteria.type === "like") {
-                    var regex = new RegExp(filter_criteria.value);
-                    match_filter[filter_criteria.field] = { "$regex": regex, "$options": "i" };
-                } else if (filter_criteria.type === "id") {
-                    match_filter[filter_criteria.field] = { "$eq": new ObjectId(filter_criteria.value) };
-                }
-            });
-        }
+// router.post('/:campaign_id',async(req,res) => {
+//     var schema = {
+//         'page_size': {
+//             notEmpty: true,
+//             errorMessage: "Page size is required"
+//         },
+//         'page_no': {
+//             notEmpty: true,
+//             errorMessage: "Page number is required"
+//         }
+//     };
+//     req.checkBody(schema);
+//     const errors = req.validationErrors();
+//     if (!errors) {
+//         var match_filter = {};
+//         var sort = {};
+//         if (req.body.filter) {
+//             req.body.filter.forEach(filter_criteria => {
+//                 if (filter_criteria.type === "exact") {
+//                     match_filter[filter_criteria.field] = filter_criteria.value;
+//                 } else if (filter_criteria.type === "between") {
+//                     if (filter_criteria.field === "age") {
+//                         // Age is derived attribute and need to calculate based on date of birth
+//                         match_filter[filter_criteria.field] = {
+//                             "$lte": moment().subtract(filter_criteria.min_value, "years").toDate(),
+//                             "$gte": moment().subtract(filter_criteria.max_value, "years").toDate()
+//                         };
+//                     } else {
+//                         match_filter[filter_criteria.field] = { "$lte": filter_criteria.min_value, "$gte": filter_criteria.max_value };
+//                     }
+//                 } else if (filter_criteria.type === "like") {
+//                     var regex = new RegExp(filter_criteria.value);
+//                     match_filter[filter_criteria.field] = { "$regex": regex, "$options": "i" };
+//                 } else if (filter_criteria.type === "id") {
+//                     match_filter[filter_criteria.field] = { "$eq": new ObjectId(filter_criteria.value) };
+//                 }
+//             });
+//         }
 
-        if (req.body.sort) {
-            req.body.sort.forEach(sort_criteria => {
-                sort[sort_criteria.field] = sort_criteria.value;
-            });
-        }
+//         if (req.body.sort) {
+//             req.body.sort.forEach(sort_criteria => {
+//                 sort[sort_criteria.field] = sort_criteria.value;
+//             });
+//         }
 
-        if (Object.keys(sort).length === 0) {
-            sort["_id"] = 1;
-        }
+//         if (Object.keys(sort).length === 0) {
+//             sort["_id"] = 1;
+//         }
 
-        let keys = {
-            "fb_friends": "user.facebook.no_of_friends",
-            "insta_followers": "user.instagram.no_of_followers",
-            "twitter_followers": "user.twitter.no_of_followers",
-            "pinterest_followers": "user.pinterest.no_of_followers",
-            "linkedin_connection": "user.linkedin.no_of_connections",
-            "year_in_industry": "user.experience",
-            "age": "user.date_of_birth",
+//         let keys = {
+//             "fb_friends": "user.facebook.no_of_friends",
+//             "insta_followers": "user.instagram.no_of_followers",
+//             "twitter_followers": "user.twitter.no_of_followers",
+//             "pinterest_followers": "user.pinterest.no_of_followers",
+//             "linkedin_connection": "user.linkedin.no_of_connections",
+//             "year_in_industry": "user.experience",
+//             "age": "user.date_of_birth",
 
-            "name": "user.name",
-            "gender": "user.gender",
-            "location": "user.location",
-            "job_industry": "user.job_industry",
-            "education": "user.education",
-            "language": "user.language",
-            "ethnicity": "user.ethnicity",
-            "interested_in": "user.interested_in",
-            "relationship_status": "user.relationship_status",
-            "music_taste": "user.music_taste"
-        };
+//             "name": "user.name",
+//             "gender": "user.gender",
+//             "location": "user.location",
+//             "job_industry": "user.job_industry",
+//             "education": "user.education",
+//             "language": "user.language",
+//             "ethnicity": "user.ethnicity",
+//             "interested_in": "user.interested_in",
+//             "relationship_status": "user.relationship_status",
+//             "music_taste": "user.music_taste"
+//         };
 
-        match_filter = await global_helper.rename_keys(match_filter, keys);
-        sort = await global_helper.rename_keys(sort, keys);
+//         match_filter = await global_helper.rename_keys(match_filter, keys);
+//         sort = await global_helper.rename_keys(sort, keys);
 
-        let campaign_post = await campaign_helper.get_applied_post_of_campaign(req.params.campaign_id,req.body.page_no,req.body.page_size,match_filter,sort);
+//         let campaign_post = await campaign_helper.get_applied_post_of_campaign(req.params.campaign_id,req.body.page_no,req.body.page_size,match_filter,sort);
 
-        if (campaign_post.status === 1) {
-            res.status(config.OK_STATUS).json({ "status": 1, "message": "Campaign details found", "results": campaign_post.campaign });
-        } else if (campaign_post.status === 2) {
-            res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Campaign not found" });
-        } else {
-            res.status(config.INTERNAL_SERVER_ERROR).json({ "status": 0, "message": "Error occured during fetching campaign details", error: campaign_post.error });
-        }
+//         if (campaign_post.status === 1) {
+//             res.status(config.OK_STATUS).json({ "status": 1, "message": "Campaign details found", "results": campaign_post.campaign });
+//         } else if (campaign_post.status === 2) {
+//             res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Campaign not found" });
+//         } else {
+//             res.status(config.INTERNAL_SERVER_ERROR).json({ "status": 0, "message": "Error occured during fetching campaign details", error: campaign_post.error });
+//         }
 
-    } else {
-        res.status(config.BAD_REQUEST).json({ message: errors });
-    }
-});
+//     } else {
+//         res.status(config.BAD_REQUEST).json({ message: errors });
+//     }
+// });
 
 /**
  * Get details of campaign by campaign_id
- * /promoter/campaign/:campaign_id/campaign_users
+ * /promoter/campaign/:campaign_id
  * Developed by "ar"
  * 
  * Not in used for now
  */
-router.post('/:campaign_id/campaign_users', async (req, res) => {
+router.post('/:campaign_id', async (req, res) => {
     var schema = {
         'page_size': {
             notEmpty: true,
