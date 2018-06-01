76ab3410b4c4a06a3a2d9bec6adbf50775a43898
ar-narola <ar@narola.email>
2018-06-01 17:53:14 +0530

    no message

--- routes/cron.js
+++ routes/cron.js
@@ -17,21 +17,25 @@ cron.schedule('0 * * * *', async () => {
 });
 
 let campaign_update = async () => {
-    console.log("this is called ------------------------------------------");
-    var campaign_helper = require('./../helpers/campaign_helper');
-    let campaign_resp = await campaign_helper.get_all_campaign();
-    if (campaign_resp.status === 1) {
-        let dir = './uploads/campaign/';
-        campaign_resp.campaigns.forEach(async (campaign) => {
-            if (campaign.cover_image) {
-                if (fs.existsSync(dir + campaign.cover_image)) {
-                    console.log("image available");
-                    var thumbnail1 = await sharp(dir + campaign.cover_image)
-                        .resize(512, 384)
-                        .toFile(dir + '512X384/' + campaign.cover_image);
+    try {
+        console.log("this is called ------------------------------------------");
+        var campaign_helper = require('./../helpers/campaign_helper');
+        let campaign_resp = await campaign_helper.get_all_campaign();
+        if (campaign_resp.status === 1) {
+            let dir = './uploads/campaign/';
+            campaign_resp.campaigns.forEach(async (campaign) => {
+                if (campaign.cover_image) {
+                    if (fs.existsSync(dir + campaign.cover_image)) {
+                        console.log("image available");
+                        var thumbnail1 = await sharp(dir + campaign.cover_image)
+                            .resize(512, 384)
+                            .toFile(dir + '512X384/' + campaign.cover_image);
+                    }
                 }
-            }
-        });
+            });
+        }
+    } catch (err) {
+        console.log("error in sharp ===> ",err);
     }
 };
 
