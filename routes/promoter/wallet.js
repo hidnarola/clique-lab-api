90ef44318b2179e2c3d22c34eff125bb1ef15cac
ar-narola <ar@narola.email>
2018-06-14 16:25:13 +0530

    wallet balance changes

--- routes/promoter/wallet.js
+++ routes/promoter/wallet.js
@@ -120,13 +120,14 @@ router.get('/bank_account', async (req, res) => {
                     });
                 }
 
+                if (promoter_resp.promoter.wallet_balance === undefined) {
+                    promoter_resp.promoter.wallet_balance = 0;
+                }
+
                 if (bank_account.length > 0) {
-                    if (promoter_resp.promoter.wallet_balance === undefined) {
-                        promoter_resp.promoter.wallet_balance = 0;
-                    }
                     res.status(config.OK_STATUS).json({ "status": 1, "message": "Bank account found", "bank_account": bank_account, "wallet_balance":promoter_resp.promoter.wallet_balance });
                 } else {
-                    res.status(config.BAD_REQUEST).json({ "status": 0, "message": "No bank account found" });
+                    res.status(config.BAD_REQUEST).json({ "status": 0, "message": "No bank account found","wallet_balance":promoter_resp.promoter.wallet_balance });
                 }
             } catch (err) {
                 res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Error occured while finding bank account", "error": err });
