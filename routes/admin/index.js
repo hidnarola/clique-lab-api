30a421e8156695d261095de19c17769fc8fe0390
ar-narola <ar@narola.email>
2018-07-04 10:55:49 +0530

    suspend user

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -7,6 +7,39 @@ var transaction_helper = require('./../../helpers/transaction_helper');
 
 var config = require('./../../config');
 
+
+router.post('/transactions', async (req, res) => {
+    var schema = {
+        'page_size': {
+            notEmpty: true,
+            errorMessage: "Page size is required"
+        },
+        'page_no': {
+            notEmpty: true,
+            errorMessage: "Page number is required"
+        }
+    };
+    req.checkBody(schema);
+    const errors = req.validationErrors();
+    if (!errors) {
+        var filter = {};
+        if (req.body.search) {
+            var regex = new RegExp(req.body.search);
+            filter["campaign_post.desription"] = { "$regex": regex, "$options": "i" };
+        }
+
+        let transaction_resp = await transaction_helper.get_all_transaction(filter, { "created_at": -1 }, req.body.page_no, req.body.page_size);
+
+        if (transaction_resp.status === 1) {
+            res.status(config.OK_STATUS).json({ "status": 1, "message": "Transactions found", "results": transaction_resp.results });
+        } else {
+            res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Transaction not found" });
+        }
+    } else {
+        res.status(config.BAD_REQUEST).json({ message: errors });
+    }
+});
+
 /**
  * 
  */
@@ -24,7 +57,7 @@ router.post('/users', async (req, res) => {
     req.checkBody(schema);
     const errors = req.validationErrors();
     if (!errors) {
-        var match_filter = {  };
+        var match_filter = {};
         var sort = {};
         if (req.body.filter) {
             req.body.filter.forEach(filter_criteria => {
@@ -82,36 +115,18 @@ router.post('/users', async (req, res) => {
     }
 });
 
-router.post('/transactions', async (req, res) => {
-    var schema = {
-        'page_size': {
-            notEmpty: true,
-            errorMessage: "Page size is required"
-        },
-        'page_no': {
-            notEmpty: true,
-            errorMessage: "Page number is required"
-        }
-    };
-    req.checkBody(schema);
-    const errors = req.validationErrors();
-    if (!errors) {
-        var filter = {};
-        if(req.body.search){
-            var regex = new RegExp(req.body.search);
-            filter["campaign_post.desription"] = { "$regex": regex, "$options": "i" };
-        }
-
-        let transaction_resp = await transaction_helper.get_all_transaction(filter,{"created_at":-1},req.body.page_no,req.body.page_size);
-
-        if(transaction_resp.status === 1){
-            res.status(config.OK_STATUS).json({"status":1,"message":"Transactions found","results":transaction_resp.results});
-        } else {
-            res.status(config.BAD_REQUEST).json({"status":0,"message":"Transaction not found"});
-        }
-    } else {
-        res.status(config.BAD_REQUEST).json({ message: errors });
+router.get('/user/suspend/:user_id', async (req, res) => {
+    var update_obj = {
+        "status":false
     }
-})
+
+    let update_resp = await user_helper.update_user_by_id(req.params.user_id,update_obj)
+
+    if(update_resp.status === 1){
+        res.status(config.OK_STATUS).json({"status":1,"message":"User has been suspended"});
+    } else {
+        res.status(config.BAD_REQUEST).json({"status":0,"message":"Can't suspend this user"});
+    }
+});
 
 module.exports = router;
