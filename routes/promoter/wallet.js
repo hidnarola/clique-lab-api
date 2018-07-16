9e5eb5a7b31d2ec4a2fcd900b77a4d01a7e9a2ce
ar-narola <ar@narola.email>
2018-07-16 15:15:53 +0530

    no message

--- routes/promoter/wallet.js
+++ routes/promoter/wallet.js
@@ -40,7 +40,7 @@ router.get('/balance', async (req, res) => {
 router.get('/cards', async (req, res) => {
     let promoter_resp = await promoter_helper.get_promoter_by_id(req.userInfo.id);
     if (promoter_resp.status === 1 && promoter_resp.promoter.stripe_customer_id) {
-        let cards = await stripe.customers.listCards(promoter_resp.promoter.stripe_customer_id);
+        let cards = await stripe.customers.listCards(promoter_resp.promoter.stripe_customer_id,{"limit":100});
         res.status(config.OK_STATUS).json({ "status": 1, "message": "Card available", "cards": cards.data });
     } else {
         console.log("resp => ", promoter_resp);
